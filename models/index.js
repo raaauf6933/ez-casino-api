ee08e2d05aaca82cbc580823aec47db06c81c248
raaauf6933 <adimaampao@stratpoint.com>
2022-06-16 18:37:10 +0800

    test conffig

--- models/index.js
+++ models/index.js
@@ -8,6 +8,8 @@ require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js");
 
+console.log(config);
+
 let sequelize = new Sequelize(
   config.database,
   config.username,
