fb3f320f86ae465cb9f6994ee3dbd9cdc11543a5
raaauf6933 <adimaampao@stratpoint.com>
2022-08-04 15:42:16 +0800

    added payout list to super user

--- src/controller/Payout/get_payout_list.js
+++ src/controller/Payout/get_payout_list.js
@@ -1,6 +1,8 @@
 const db = require("../../../models");
 const { returnList } = require("../../common/returnResponse");
+const { userTypes } = require("../../enum");
 const Payout = db.payOutBatch;
+const Club = db.club;
 const User = db.users;
 
 const GetPayoutList = async (req, res) => {
@@ -10,20 +12,47 @@ const GetPayoutList = async (req, res) => {
     foreignKey: "added_by",
   });
 
+  Payout.belongsTo(Club, {
+    foreignKey: "club_id",
+  });
+
   try {
-    const result = await Payout.findAll({
-      include: {
-        model: User,
-        attributes: ["id", "club_id", "first_name", "last_name"],
-      },
-      where: {
-        club_id: user.club_id,
-      },
-    });
+    if (user.usertype === userTypes.SUPER_USER) {
+      const result = await Payout.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["id", "club_id", "first_name", "last_name"],
+          },
+          {
+            model: Club,
+            attributes: ["id", "club_name"],
+          },
+        ],
+      });
+      const payouts = result.map((e) => e.toJSON());
 
-    const payouts = result.map((e) => e.toJSON());
+      res.send(returnList(payouts));
+    } else {
+      const result = await Payout.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["id", "club_id", "first_name", "last_name"],
+          },
+          {
+            model: Club,
+            attributes: ["id", "club_name"],
+          },
+        ],
+        where: {
+          club_id: user.club_id,
+        },
+      });
+      const payouts = result.map((e) => e.toJSON());
 
-    res.send(returnList(payouts));
+      res.send(returnList(payouts));
+    }
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
