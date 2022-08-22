d4e4ed78c20607d67ffb6fae2eb4ae737bc6455b
raaauf6933 <adimaampao@stratpoint.com>
2022-08-22 13:11:03 +0800

    remove status

--- migrations/20220818112451-create-club-payout-batches.js
+++ migrations/20220818112451-create-club-payout-batches.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('club_payout_batches', {
+    await queryInterface.createTable("club_payout_batches", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       total_club_fee: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
       },
       added_by: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
-      status: {
-        type: Sequelize.STRING
-      },
+      // status: {
+      //   type: Sequelize.STRING
+      // },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('club_payout_batches');
-  }
+    await queryInterface.dropTable("club_payout_batches");
+  },
 };
