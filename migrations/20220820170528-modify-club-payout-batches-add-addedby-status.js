ccd000cf11f98af573e545431ba49abdb1b04029
raaauf6933 <adimaampao@stratpoint.com>
2022-08-22 13:12:28 +0800

    remove status

--- migrations/20220820170528-modify-club-payout-batches-add-addedby-status.js
+++ migrations/20220820170528-modify-club-payout-batches-add-addedby-status.js
@@ -2,15 +2,15 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn(
-      "club_payout_batches", // table name
-      "status", // new field name
-      {
-        type: Sequelize.STRING,
-        allowNull: true,
-        after: "total_club_fee",
-      }
-    );
+    // await queryInterface.addColumn(
+    //   "club_payout_batches", // table name
+    //   "status", // new field name
+    //   {
+    //     type: Sequelize.STRING,
+    //     allowNull: true,
+    //     after: "total_club_fee",
+    //   }
+    // );
     await queryInterface.addColumn(
       "club_payout_batches", // table name
       "added_by", // new field name
@@ -23,7 +23,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("club_payout_batches", "status");
+    // await queryInterface.removeColumn("club_payout_batches", "status");
     await queryInterface.removeColumn("club_payout_batches", "added_by");
   },
 };
